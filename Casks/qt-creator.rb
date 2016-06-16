b80f95283e3ce5d17205b95ac586651d7e114b8c
Jeff Scholen <jmscholen@users.noreply.github.com>
2016-06-16 10:47:49 -0400

    Update qt-creator.rb
    require https to access download.

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -2,7 +2,7 @@ cask 'qt-creator' do
   version '4.0.1'
   sha256 '726da47bc13fed63aea34156bb58ab3b05217e51adeb07d50cecd59c9c57509f'
 
-  url "http://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  url "https://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
   homepage 'https://www.qt.io/developers/'
   license :gpl
